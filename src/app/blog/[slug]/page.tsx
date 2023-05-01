2ceb9d48d7e8f732c1256a479a62798b5f622b9b
jwmccowan <github@jwmccowan.com>
2023-05-01 23:55:18 +1000

    add content and revalidation to post page

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,4 +1,14 @@
 import { allPosts } from "contentlayer/generated";
+import { format, parseISO } from "date-fns";
+import { notFound } from "next/navigation";
+
+export const dynamic = "error";
+export const dynamicParams = true;
+export const revalidate = 60;
+
+export function generateStaticParams() {
+  return allPosts.map((post) => post._id.split(".")[0]);
+}
 
 interface PageProps {
   params: { slug: string };
@@ -7,11 +17,14 @@ interface PageProps {
 export default function BlogPostPage({ params: { slug } }: PageProps) {
   const post = allPosts.find((post) => post._raw.flattenedPath === slug);
   if (!post) {
-    throw new Error();
+    throw notFound();
   }
   return (
-    <div className="container mx-auto px-4">
+    <div className="prose my-12">
       <h1 className="text-3xl font-bold">{post.title}</h1>
+      <p className="text-gray-600 italic">
+        {format(parseISO(post.date), "LLLL d, yyyy")}
+      </p>
       <div dangerouslySetInnerHTML={{ __html: post.body.html }} />
     </div>
   );
