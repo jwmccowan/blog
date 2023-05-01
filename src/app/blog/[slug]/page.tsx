fceeb5c65f4c1dba0577d516f1fa177e7b0a6f6d
jwmccowan <github@jwmccowan.com>
2023-05-01 23:24:22 +1000

    add post page

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,7 +1,18 @@
+import { allPosts } from "contentlayer/generated";
+
 interface PageProps {
   params: { slug: string };
 }
 
 export default function BlogPostPage({ params: { slug } }: PageProps) {
-  return <div>{slug}</div>;
+  const post = allPosts.find((post) => post._raw.flattenedPath === slug);
+  if (!post) {
+    throw new Error();
+  }
+  return (
+    <div className="container mx-auto px-4">
+      <h1 className="text-3xl font-bold">{post.title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: post.body.html }} />
+    </div>
+  );
 }
